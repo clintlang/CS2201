eaf410406075ea12c0b334e705d1f05b14c10d93
Clint Lang <clinton.thomas.lang@gmail.com>
2023-02-07 15:49:43 -0600

    this is project 2, if it says its 1 it isn't

---
+++ project2/cmake-build-debug/CMakeFiles/project2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2.dir/DNA_Strand.cpp.o"
+  "CMakeFiles/project2.dir/DNA_Strand.cpp.o.d"
+  "CMakeFiles/project2.dir/DNAtest.cpp.o"
+  "CMakeFiles/project2.dir/DNAtest.cpp.o.d"
+  "project2"
+  "project2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
