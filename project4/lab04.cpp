c5273615ec6ba3266eaf46bb729c71a5e41912a3
Clint Lang <clinton.thomas.lang@gmail.com>
2023-02-27 20:39:34 -0600

    Project 4

--- project4/lab04.cpp
+++ project4/lab04.cpp
@@ -20,8 +20,82 @@
 #include <utility>
 #include <cmath>
 #include "lab04.h"
- 
 
-// write your recursive functions here
 
+int sumArray(int anArray[], size_t size)
+{
+
+}
+
+
+bool member(int target, const int set[], size_t size)
+{
+    if (size == 0) {
+        return false;
+    }
+
+    if (set[size - 1] == target) {
+        return true;
+    }
+
+    return member(target, set, size - 1);
+}
+
+
+bool isPalindrome(const std::string &str)
+{
+    if (str.length() == 0) {
+        return true;
+    }
+
+    if (tolower(str[0]) == tolower(str[str.length() - 1])) {
+        return isPalindrome(str.substr(1, str.length() - 2));
+    }
+
+    return false;
+}
+
+
+double harmonicSum(size_t n)
+{
+    if (n == 0) {
+        return 0;
+    }
+
+    return ((1.0 / (double)n) + harmonicSum(n - 1));
+}
+
+
+std::string addStar(const std::string &str)
+{
+    if (str.length() == 0 || str.length() == 1) {
+        return str;
+    }
+
+    std::string newStr(str);
+    newStr.insert(1, "*");
+
+    return newStr.substr(0, 2) + addStar(newStr.substr(2, newStr.length() - 1));
+}
+
+
+size_t replace(int target, int replacement, int numbers[], size_t size)
+{
+    if (size == 0) {
+        return 0;
+    }
+
+    if (numbers[size - 1] == target) {
+        numbers[size - 1] = replacement;
+        return 1 + replace(target, replacement, numbers, size - 1);
+    }
+
+    return replace(target, replacement, numbers, size - 1);
+}
+
+
+size_t gcd(size_t x, size_t y)
+{
+
+}
 
